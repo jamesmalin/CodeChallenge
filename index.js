a2b538a623ba208cd8c2c84f33bfbb492098e0a8
James Malin <jamesmalin@users.noreply.github.com>
2019-02-21 22:29:25 -0800

    Initial upload

---
+++ index.js
@@ -0,0 +1,76 @@
+const fs = require('fs');
+const readlineSync = require('readline-sync');
+const path = require('path');
+const mkdirp = require('mkdirp');
+const rimraf = require('rimraf');
+const dirTree = require('directory-tree');
+const fsExtra = require('fs-extra');
+
+console.log('Welcome. Please use one of the following commands: "CREATE", "LIST", "MOVE", "DELETE". \
+	Type "exit" to quit.');
+
+readlineSync.promptCLLoop({
+  CREATE: function(target) {
+ 	if (fs.existsSync(target)){
+     	console.log(target + ' already exists.');
+ 	} else {
+ 		try{
+		    mkdirp.sync('./'+target, function (err) {
+			    if (err) console.error(err)
+			});
+		    console.log('CREATE ' + target);
+		}
+		catch(e){
+		    //do whatever with error
+		    console.log('There was an error moving ' + target + ' into ' + into);
+		}
+ 	}
+  },
+  MOVE: function(target, into) {
+	try{
+		var endTarget = target.split(/[\s/]+/);
+
+	    fsExtra.moveSync('./'+target, './'+into+'/'+endTarget[endTarget.length-1], err => {
+		  if(err) return console.error(err);
+		  console.log('success!');
+		});
+	    console.log('MOVED ' + endTarget[endTarget.length-1]);
+	}
+	catch(e){
+	    //do whatever with error
+	    console.log('There was an error moving ' + target + ' into ' + into);
+	}
+
+  },
+  LIST: function(target, into) {
+	const tree = dirTree('./', {exclude:/node_modules/}, null, (item, PATH, stats) => {
+	    var tab = "  ";
+		var count = (item.path.match(/\\/g) || []).length;
+		function repeatStringNumTimes(string, times) {
+		  return times > 0 ? string.repeat(times) : "";
+		}
+		if (item.path !== './') {
+			console.log(repeatStringNumTimes(tab, count) + item.name);
+		}
+	});
+  },
+  DELETE: function(target) {
+  	//refactor
+  	try {
+	    fs.statSync(target);
+		try{
+		    rimraf.sync('./'+target)
+		    console.log('DELETE ' + target);
+		}
+		catch(err){
+		    //do whatever with error
+		    console.log(err);
+		}
+	  } catch(e) {
+	    console.log('Cannot delete ' + target + ' - ' + target + ' does not exist');
+	  }
+  },
+  exit: function() { return true; }
+});
+
+console.log('Exited');
---
+++ readme.md
@@ -0,0 +1,5 @@
+1. Make sure you have Node.js installed on your machine: https://nodejs.org/en/
+
+2. Install dependencies using ```npm install``` from the root directory.
+
+3. Available commands: "CREATE", "LIST", "MOVE", "DELETE". Type "exit" to quit.
